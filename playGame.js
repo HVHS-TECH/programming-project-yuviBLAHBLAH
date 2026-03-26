248f2814a7db64cc8193be3e2d4cc7c63b02687e
Your Name <23096yb@hvhs.school.nz>
2026-03-27 09:58:26 +1300

    im fried yo.

--- playGame.js
+++ playGame.js
@@ -13,106 +13,92 @@ function preload() {
 }
 
 let score = 0
-let gameState = "menu";
 /*******************************************************/
 // setup()
 /*******************************************************/
-function deleteBall(_alien, _horse) {
-
-	// Delete the alien which was hit
-	console.log("colliding: ");
-	_alien.remove();
-
-}
 function setup() {
 
-	//create a new group for the aliens and define the properties of the aliens
+	// testing stuff here
 
-	alienGroup = new Group();
+	//set framerate as 120
 	frameRate(120)
 	console.log("setup: ");
 	cnv = new Canvas(windowWidth, windowHeight);
-
+	// set world gravity
 	world.gravity.y = 4.5;
-
+	// make borders
 	wallTop = new Sprite(width / 2, height - height, width, 10, 'k');
 	wallTop.color = 'white';
 
 	wallBot = new Sprite(width / 2, height, width, 10, 'k');
 	wallBot.color = 'white';
+	// create horse
 	horse = new Sprite(width / 2 - 25, height - 700, 50, 'd');
-
+	// define horse properties
 	horse.bounciness = 0.1;
 	horse.speed = 0;
-
+	horse.gravity = 10;
 	horse.image = (imgHorsi);
 
 	imgHorsi.resize(50, 50);
 
-	pillarTop = new Sprite(width - 100, height - 750, 100, 350, 'k')
-	pillarBot = new Sprite(width - 100, height - 50, 100, 350, 'k')
-	pillarBot.moveTowards(horse, 0.001, 'k');
-	pillarTop.moveTowards(horse, 0.001, 'k');
+	anchor = new Sprite(windowWidth - 5000, 500)
+	// make moving pillars towards left side of screen
+	pillarTop = new Sprite(width + 100, height - 750, 100, 450, 'k')
+	pillarBot = new Sprite(width + 100, height - 50, 100, 450, 'k')
+	pillarBot.moveTowards(anchor, 0.001, 'k');
+	pillarTop.moveTowards(anchor, 0.001, 'k');
 
+
+}
+/*******************************************************/
+// draw()
+/*******************************************************/
+function draw() {
+	// draw background
+	background(imgBG);
+	// draw score
+	textSize(32);
+	fill(255);
+	stroke(0);
+	strokeWeight(4);
+	text(score, windowWidth / 2 - 40, 150);
+	//the score count
+	score = frameCount / 120;
+	score = Math.floor(score)
+	console.log("score: " + score);
+	// horse controls, if the keyboard presses up then set horse speed 2.5 towards -90 degrees
+	if (kb.pressing('up')) {
+		horse.speed = 2.5;
+	};
+
+	if (kb.pressing('up')) {
+		horse.direction = -90;
+	};
+
+	if (kb.pressing('up')) {
+
+		horse.speed = 2.5;
+	};
+	// if the horse collides with the pillars then set world time to 0
 	if (horse.collides(pillarBot)) {
 		world.timeScale = 0;
 	}
 	else {
 		world.timeScale = 1;
 
-	}
-	for (i = 10; i < 20; i++) {
+	};
+// when horse collides with pillars display text showing that the game is over
+	if (horse.collides(pillarTop)) {
+		world.timeScale = 0;
+		text("you lost!, refresh tab to restart", windowWidth / 2 - 100, windowHeight / 6);
+	};
+	if (horse.collides(pillarBot)) {
+		world.timeScale = 0;
+		text("you lost!, refresh tab to restart", windowWidth / 2 - 100, windowHeight / 6);
+	};
 
-		let alien = new Sprite(windowWidth / 2, windowHeight / 4, 100, 100);
 
-		alien.vel.x = 3;
-
-		alien.vel.y = 4;
-
-		alien.bounciness = 1;
-
-		alien.friction = 0;
-
-		alienGroup.add(alien);
-
-		alienGroup.collides(horse, deleteBall);
-	}
-	
-}
-/*******************************************************/
-// draw()
-/*******************************************************/
-function draw() {
-	background(imgBG);
-	textSize(32);
-	fill(255);
-	stroke(0);
-	strokeWeight(4);
-	text(score, windowWidth / 2 - 40, 150);
-	//the score
-	score = frameCount / 120;
-	score = Math.floor(score)
-	console.log("score: " + score);
-	if (kb.pressing('up')) {
-
-		horse.speed = 2.5;
-
-		if (kb.pressing('up')) {
-			horse.direction = -90;
-		} else {
-			horse.speed = 0;
-		}
-	}
-
-		if (kb.pressing('up')) {
-
-		horse.speed = 2.5;
-}
-		if (kb.pressing('r')) {
-			world.timeScale = 0;
-		} else {
-			world.timeScale = 1;
-		}
 
 }
 
